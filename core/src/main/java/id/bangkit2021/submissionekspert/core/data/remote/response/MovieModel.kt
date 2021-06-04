6b222c8492b5d48240a4cc5a9343a41419a7bb38
hendrawanwib <hendrawanwib@gmail.com>
2021-06-04 13:11:06 +0700

    add commit

--- core/src/main/java/id/bangkit2021/submissionekspert/core/data/remote/response/MovieModel.kt
+++ core/src/main/java/id/bangkit2021/submissionekspert/core/data/remote/response/MovieModel.kt
@@ -1,10 +1,18 @@
 package id.bangkit2021.submissionekspert.core.data.remote.response
 
+import com.google.gson.annotations.SerializedName
+
 data class MovieModel(
+    @SerializedName("id")
     var id: Int,
+    @SerializedName("poster_path")
     var poster_path: String,
+    @SerializedName("original_title")
     var original_title: String,
+    @SerializedName("release_date")
     var release_date: String,
+    @SerializedName("overview")
     var overview: String,
+    @SerializedName("backdrop_path")
     var backdrop_path: String,
 )
--- core/src/main/java/id/bangkit2021/submissionekspert/core/data/remote/response/MovieResponse.kt
+++ core/src/main/java/id/bangkit2021/submissionekspert/core/data/remote/response/MovieResponse.kt
@@ -1,5 +1,8 @@
 package id.bangkit2021.submissionekspert.core.data.remote.response
 
+import com.google.gson.annotations.SerializedName
+
 data class MovieResponse(
+    @SerializedName("results")
     val results: List<MovieModel>
 )
